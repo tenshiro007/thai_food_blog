fff2b654eac421aa794461c2a28a947ffa7cc166
Waritthon <wra@ii.co.th>
2023-07-23 22:18:34 +0700

    update add user success

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,16 +1,33 @@
 const User=require('../models/user')
 
-const create=(req,res)=>{
-    res.send('create')
+const create=async(req,res)=>{
+    try{
+        const {name, fname, lname, username, email, password, intro} =req.body;
+        const user=await User.create(name, fname, lname, username, email, password, intro)
+        res.send(user)
+    }catch(err){
+        console.log(JSON.stringify(err));
+        res.status(500).send(err)
+    }
 }
 
-const getAll=(req,res)=>{
-    console.log(req.body);
-    res.send('getAll')
+const getAll=async(req,res)=>{
+    try{
+        const user=await User.getAll();
+        res.send(user)
+    }catch(err){
+        console.log(JSON.stringify(err));
+        res.status(500).send(err)
+    }
 }
-const get=(req,res)=>{
-    console.log(req.body);
-    res.send('get')
+const get=async(req,res)=>{
+    try{
+        const user=await User.get(req.params.id)
+        res.send(user)
+    }catch(err){
+        console.log(JSON.stringify(err));
+        res.status(500).send(err)
+    }
 }
 
 module.exports={
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router()
 const userController=require('../controllers/user')
 
 router.get('/',userController.getAll);
-// router.get('/:id',userController.get);
+router.get('/:id',userController.get);
+router.post('/',userController.create);
 
 module.exports=router;
